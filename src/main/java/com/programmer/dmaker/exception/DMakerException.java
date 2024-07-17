fa3f37db5558d800b36d3cade15bc28f24b49739
hunit <hunit612@naver.com>
2024-07-17 23:57:25 +0900

    validation

---
+++ src/main/java/com/programmer/dmaker/exception/DMakerException.java
@@ -0,0 +1,19 @@
+package com.programmer.dmaker.exception;
+
+public class DMakerException extends RuntimeException {
+    private DMakerErrorCode dMakerErrorCode;
+    private String detailMessage;
+
+    public DMakerException(DMakerErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.dMakerErrorCode = errorCode;
+        this.detailMessage = errorCode.getMessage();
+    }
+
+    public DMakerException(DMakerErrorCode errorCode, String detailMessage) {
+        super(detailMessage);
+        this.dMakerErrorCode = errorCode;
+        this.detailMessage = detailMessage;
+    }
+
+}
--- src/main/java/com/programmer/dmaker/service/DMakerService.java
+++ src/main/java/com/programmer/dmaker/service/DMakerService.java
@@ -2,12 +2,19 @@ package com.programmer.dmaker.service;
 
 import com.programmer.dmaker.dto.CreateDeveloper;
 import com.programmer.dmaker.entity.Developer;
+import com.programmer.dmaker.exception.DMakerErrorCode;
+import com.programmer.dmaker.exception.DMakerException;
 import com.programmer.dmaker.repository.DeveloperRepository;
 import com.programmer.dmaker.type.DeveloperLevel;
 import com.programmer.dmaker.type.DeveloperSkillType;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static com.programmer.dmaker.exception.DMakerErrorCode.DUPLICATED_MEMBER_ID;
+import static com.programmer.dmaker.exception.DMakerErrorCode.LEVEL_EXPERIENCE_YEARS_NOT_MATCHED;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +23,8 @@ public class DMakerService {
 
     @Transactional
     public void createDeveloper(CreateDeveloper.Request request) {
+        validateCreateDeveloperRequest(request);
+
         Developer developer = Developer.builder()
                 .developerLevel(DeveloperLevel.JUNIOR)
                 .developerSkillType(DeveloperSkillType.FRONT_END)
@@ -25,5 +34,28 @@ public class DMakerService {
                 .build();
 
         developerRepository.save(developer);
+    }
+
+    private void validateCreateDeveloperRequest(CreateDeveloper.Request request) {
+        DeveloperLevel developerLevel = request.getDeveloperLevel();
+        Integer exprienceYears = request.getExprienceYears();
+        if (developerLevel == DeveloperLevel.SENIOR
+         && exprienceYears < 10) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        if (developerLevel == DeveloperLevel.JUNGNIOR
+        && (exprienceYears < 4 || exprienceYears > 10)) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        if (developerLevel == DeveloperLevel.JUNIOR && exprienceYears > 4) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+        Optional<Developer> developer = developerRepository.findByMemberId(request.getMemberId());
+        if (developer.isPresent()) {
+            throw new DMakerException(DUPLICATED_MEMBER_ID);
+        }
     }
 }
